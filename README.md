94e4b3d9e4eb422b7c05f88aa023fb3c3399f073
shubhamgupta26 <shubham2604gupta@gmail.com>
2019-02-09 02:35:52 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Webpack-React
+
+Understand bundling in react.
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const htmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  mode: "development",
+  plugins: [
+    new htmlWebPackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
